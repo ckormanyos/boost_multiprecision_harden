ca0c15e7ab7e4f8b659292a5d5b22f068e85b124
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-05-04 15:59:44 +0200

    Try first batch of mpfr warns

--- test.cpp
+++ test.cpp
@@ -13,7 +13,7 @@
 #endif
 
 // cd /mnt/c/Users/User/Documents/Ks/PC_Software/NumericalPrograms/ExtendedNumberTypes/boost_multiprecision_examples/boost_multiprecision_harden
-// g++ -Wall -Wextra -Wconversion -Wsign-conversion -Wshadow -Wundef -Wunused-parameter -Wuninitialized -Wunreachable-code -Winit-self -Wzero-as-null-pointer-constant -O3 -std=c++11 -I/mnt/c/boost/modular_boost/boost/libs/config/include -I. standalone_bernoulli_tgamma_cpp_bin_float.cpp standalone_bernoulli_tgamma_cpp_dec_float.cpp standalone_bernoulli_tgamma_gmp_float.cpp test.cpp -lgmp -o test.exe
+// g++ -Wall -Wextra -Wconversion -Wsign-conversion -Wshadow -Wundef -Wunused-parameter -Wuninitialized -Wunreachable-code -Winit-self -Wzero-as-null-pointer-constant -O3 -std=c++11 -I/mnt/c/boost/modular_boost/boost/libs/config/include -I. standalone_bernoulli_tgamma_cpp_bin_float.cpp standalone_bernoulli_tgamma_cpp_dec_float.cpp standalone_bernoulli_tgamma_gmp_float.cpp standalone_bernoulli_tgamma_mpfr_float.cpp test.cpp -lgmp -lmpfr -o test.exe
 
 // Build with arm-none-eabi for ARM1176.
 // arm-none-eabi-g++ -std=c++11 -Wall -Wextra -Wconversion -Wsign-conversion -Wshadow -Wundef -Wunused-parameter -Wuninitialized -Wunreachable-code -Winit-self -Wzero-as-null-pointer-constant -O2 -g -gdwarf-2 -fno-exceptions -ffunction-sections -fdata-sections -x c++ -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -fno-enforce-eh-specs -ftemplate-depth=128 -marm -march=armv6zk -mtune=arm1176jzf-s -mfpu=vfpv2 -mfloat-abi=hard -ffast-math -DBOOST_NO_EXCEPTIONS -I./src/mcal/stm32f446 -I/mnt/c/boost/modular_boost/boost/libs/config/include -I. standalone_bernoulli_tgamma_cpp_bin_float.cpp standalone_bernoulli_tgamma_cpp_dec_float.cpp test.cpp -Wl,--gc-sections -Wl,-Map,./test.map --specs=nano.specs --specs=nosys.specs -o ./test.elf
@@ -22,6 +22,7 @@ extern auto example008_bernoulli_tgamma_cpp_bin_float() -> bool;
 extern auto example008_bernoulli_tgamma_cpp_dec_float() -> bool;
 #if !defined(__arm__)
 extern auto example008_bernoulli_tgamma_gmp_float    () -> bool;
+extern auto example008_bernoulli_tgamma_mpfr_float   () -> bool;
 #endif
 
 namespace local
@@ -52,6 +53,7 @@ auto main() -> int
   const auto result_is_ok_cpp_dec_float = local::test(example008_bernoulli_tgamma_cpp_dec_float);
   #if !defined(__arm__)
   const auto result_is_ok_gmp_____float = local::test(example008_bernoulli_tgamma_gmp_float);
+  const auto result_is_ok_mpfr____float = local::test(example008_bernoulli_tgamma_mpfr_float);
   #endif
 
   const auto result_is_ok =
@@ -60,6 +62,7 @@ auto main() -> int
     && result_is_ok_cpp_dec_float
     #if !defined(__arm__)
     && result_is_ok_gmp_____float
+    && result_is_ok_mpfr____float
     #endif
   );
 
@@ -67,6 +70,7 @@ auto main() -> int
   std::cout << "result_is_ok_cpp_bin_float: " << std::boolalpha << result_is_ok_cpp_bin_float << std::endl;
   std::cout << "result_is_ok_cpp_dec_float: " << std::boolalpha << result_is_ok_cpp_dec_float << std::endl;
   std::cout << "result_is_ok_gmp_____float: " << std::boolalpha << result_is_ok_gmp_____float << std::endl;
+  std::cout << "result_is_ok_mpfr____float: " << std::boolalpha << result_is_ok_mpfr____float << std::endl;
   std::cout << "result_is_ok (total):       " << std::boolalpha << result_is_ok               << std::endl;
   #endif
 
